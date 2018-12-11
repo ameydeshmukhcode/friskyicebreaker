c6cc7bc624e0d134595cb5f40e3dd27b7c342f28
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2018-12-12 00:09:11 +0530

    Added HomeActivity.java

--- app/src/main/java/com/frisky/icebreaker/LoginActivity.java
+++ app/src/main/java/com/frisky/icebreaker/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.frisky.icebreaker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,7 +21,8 @@ public class LoginActivity extends AppCompatActivity {
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                Intent startHomeActivity = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(startHomeActivity);
             }
         });
     }
