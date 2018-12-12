ec8b96230404e8998459063c7b3523e25dd3316d
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2018-12-12 22:50:03 +0530

    Updated ids

--- app/src/main/java/com/frisky/icebreaker/HomeActivity.java
+++ app/src/main/java/com/frisky/icebreaker/HomeActivity.java
@@ -18,14 +18,14 @@ public class HomeActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.bottom_nav_pubs:
+                    mTextMessage.setText(R.string.bottom_nav_pubs);
+                    return true;
+                case R.id.bottom_nav_home:
                     mTextMessage.setText(R.string.bottom_nav_home);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.bottom_nav_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.bottom_nav_notifications);
+                case R.id.bottom_nav_profile:
+                    mTextMessage.setText(R.string.bottom_nav_profile);
                     return true;
             }
             return false;
