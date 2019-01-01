95c952d6c802d086de0e173b738b7ef4ad7138c5
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-01-01 20:27:46 +0530

    Updated HomeActivity, Added menus, Separated toolbar

--- app/src/main/java/com/frisky/icebreaker/HomeActivity.java
+++ app/src/main/java/com/frisky/icebreaker/HomeActivity.java
@@ -3,14 +3,14 @@ package com.frisky.icebreaker;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private Toolbar mToolbarMain;
     private BottomNavigationView mBottomNavigationView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -19,14 +19,13 @@ public class HomeActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.bottom_nav_pubs:
-                    mTextMessage.setText(R.string.bottom_nav_pubs);
-                    return true;
                 case R.id.bottom_nav_home:
-                    mTextMessage.setText(R.string.bottom_nav_home);
+                    loadFragment(new PubViewFragment());
                     return true;
                 case R.id.bottom_nav_profile:
-                    mTextMessage.setText(R.string.bottom_nav_profile);
+                    loadFragment(new ProfileFragment());
+                    return true;
+                case R.id.bottom_nav_icebreaker:
                     return true;
             }
             return false;
@@ -38,15 +37,45 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        mTextMessage = findViewById(R.id.message);
+        mToolbarMain = findViewById(R.id.home_activity_toolbar);
+        mToolbarMain.setTitle(R.string.title_activity_home);
+
+        mToolbarMain.inflateMenu(R.menu.menu_home_activity);
+
+        mToolbarMain.setOnMenuItemClickListener(
+                new Toolbar.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+
+                        switch(item.getItemId()){
+                            case R.id.home_menu_scan_qr:
+                                //TODO: Start QR Scanning Activity from here
+                                return true;
+                            case R.id.home_menu_social:
+                                loadFragment(new SocialFragment());
+                                return true;
+                        }
+                        return false;
+                    }
+                });
+
+        loadFragment(new PubViewFragment());
+
         mBottomNavigationView = findViewById(R.id.navigation);
         mBottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         mBottomNavigationView.setSelectedItemId(R.id.bottom_nav_home);
     }
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_home_activity, menu);
-        return true;
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.home_activity_fragment, fragment)
+                    .commit();
+
+            return true;
+        }
+        return false;
     }
 }
