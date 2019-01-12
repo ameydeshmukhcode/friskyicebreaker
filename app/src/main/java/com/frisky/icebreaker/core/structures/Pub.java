af9c3a920a1b454a5dd70fb0930024239537906f
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-01-12 22:45:13 +0530

    Modified Pub class

--- app/src/main/java/com/frisky/icebreaker/core/structures/Pub.java
+++ app/src/main/java/com/frisky/icebreaker/core/structures/Pub.java
@@ -1,30 +1,52 @@
 package com.frisky.icebreaker.core.structures;
 
+import java.util.List;
+
 public class Pub {
     private String mName;
     private String mDesc;
+    private List<String> mTags;
+    private double mRating;
 
     public Pub() {
     }
 
-    public Pub(String mName, String mDesc) {
-        this.mName = mName;
-        this.mDesc = mDesc;
+    public Pub(String name, String desc, List<String> tags, double rating) {
+        this.mName = name;
+        this.mDesc = desc;
+        this.mTags = tags;
+        this.mRating = rating;
     }
 
-    public String getmName() {
+    public String getName() {
         return mName;
     }
 
-    public void setmName(String mName) {
+    public void setName(String mName) {
         this.mName = mName;
     }
 
-    public String getmDesc() {
+    public String getDesc() {
         return mDesc;
     }
 
-    public void setmDesc(String mDesc) {
+    public void setDesc(String mDesc) {
         this.mDesc = mDesc;
+    }
+
+    public List<String> getTags() {
+        return mTags;
+    }
+
+    public void setTags(List<String> mTags) {
+        this.mTags = mTags;
+    }
+
+    public double getRating() {
+        return mRating;
+    }
+
+    public void setRating(float mRating) {
+        this.mRating = mRating;
     }
 }
