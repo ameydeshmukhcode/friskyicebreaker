705e65bbff48e6486de2307ea4cd60927d8fc7d9
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-01-12 22:55:12 +0530

    Added UsersListViewAdapter to switch between different user interaction views, Added UserInfoFragment subclasses

---
+++ app/src/main/java/com/frisky/icebreaker/ui/social/PingsFragment.java
@@ -0,0 +1,10 @@
+package com.frisky.icebreaker.ui.social;
+
+import com.frisky.icebreaker.core.base.UserInfoFragment;
+import com.frisky.icebreaker.core.structures.UserInfoMode;
+
+public class PingsFragment extends UserInfoFragment {
+    public PingsFragment() {
+        setUserInfoMode(UserInfoMode.PING);
+    }
+}
