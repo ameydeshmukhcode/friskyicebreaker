1d2f31238c4fbde16a17b12c6d8453c9744b4c0e
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-01-15 01:06:20 +0530

    Added EditProfileActivity.java

---
+++ app/src/main/java/com/frisky/icebreaker/ui/profile/EditProfileActivity.java
@@ -0,0 +1,31 @@
+package com.frisky.icebreaker.ui.profile;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+
+import com.frisky.icebreaker.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_edit_profile_activity, menu);
+        return true;
+    }
+}
