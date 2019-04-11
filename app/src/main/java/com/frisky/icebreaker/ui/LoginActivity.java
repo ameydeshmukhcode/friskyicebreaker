1be852b94c6a472223c5f9f9228e819495a9e3b4
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-04-11 23:23:52 +0530

    Updated login activity

--- app/src/main/java/com/frisky/icebreaker/ui/LoginActivity.java
+++ app/src/main/java/com/frisky/icebreaker/ui/LoginActivity.java
@@ -5,10 +5,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.frisky.icebreaker.R;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button mLoginButton;
+    TextView mSignUpLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +22,24 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void initUI() {
-        Button mLoginButton = findViewById(R.id.button_login);
+        mLoginButton = findViewById(R.id.button_login);
+        mSignUpLink = findViewById(R.id.link_sign_up);
 
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //TODO handle Login operation here
                 Intent startHomeActivity = new Intent(getApplicationContext(), HomeActivity.class);
                 startActivity(startHomeActivity);
                 finish();
+            }
+        });
+
+        mSignUpLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startSignUpActivity = new Intent(getApplicationContext(), SignUpActivity.class);
+                startActivity(startSignUpActivity);
             }
         });
     }
