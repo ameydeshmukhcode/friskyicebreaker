94b2ccdf0262c3c3b114920c255533f54d801de5
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-04-11 23:24:18 +0530

    Added sign up activity

---
+++ app/src/main/java/com/frisky/icebreaker/ui/SignUpActivity.java
@@ -0,0 +1,34 @@
+package com.frisky.icebreaker.ui;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.frisky.icebreaker.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    Button mSignUpButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        initUI();
+    }
+
+    private void initUI() {
+        mSignUpButton = findViewById(R.id.button_sign_up);
+
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO handle SignUp operation here
+                Intent startLoginActivity = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(startLoginActivity);
+            }
+        });
+    }
+}
