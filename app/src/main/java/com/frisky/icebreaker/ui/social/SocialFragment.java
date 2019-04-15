a4728e61484c106e6261dd5bd6470cf7054fd193
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-04-16 01:25:03 +0530

    Added custom tabs to Social Fragment

--- app/src/main/java/com/frisky/icebreaker/ui/social/SocialFragment.java
+++ app/src/main/java/com/frisky/icebreaker/ui/social/SocialFragment.java
@@ -3,16 +3,19 @@ package com.frisky.icebreaker.ui.social;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.frisky.icebreaker.R;
 
 public class SocialFragment extends Fragment {
+
+    Button mChatsButton;
+    Button mPendingButton;
 
     @Nullable
     @Override
@@ -20,11 +23,26 @@ public class SocialFragment extends Fragment {
         View view;
         view = inflater.inflate(R.layout.fragment_social_view, null);
 
-        ViewPager viewPager = (ViewPager) view.findViewById(R.id.pager_social);
+        final ViewPager viewPager = view.findViewById(R.id.pager_social);
         SocialPagerAdapter myPagerAdapter = new SocialPagerAdapter(getFragmentManager());
         viewPager.setAdapter(myPagerAdapter);
-        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabs_social);
-        tabLayout.setupWithViewPager(viewPager);
+
+        mChatsButton = view.findViewById(R.id.button_chats);
+        mPendingButton = view.findViewById(R.id.button_pending);
+
+        mChatsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(0);
+            }
+        });
+
+        mPendingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(1);
+            }
+        });
 
         return view;
     }
