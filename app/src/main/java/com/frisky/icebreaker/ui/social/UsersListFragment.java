c91efb9fa2e8fab79dd5a27808d7758b99f776b6
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-04-16 01:29:20 +0530

    Added layout fragment_icebreaker_list and set it in UsersListFragment

--- app/src/main/java/com/frisky/icebreaker/ui/social/UsersListFragment.java
+++ app/src/main/java/com/frisky/icebreaker/ui/social/UsersListFragment.java
@@ -1,11 +1,21 @@
 package com.frisky.icebreaker.ui.social;
 
+import android.view.LayoutInflater;
+import android.view.View;
+
+import com.frisky.icebreaker.R;
 import com.frisky.icebreaker.core.base.UserInfoFragment;
 import com.frisky.icebreaker.core.structures.UserInfoMode;
 
 public class UsersListFragment extends UserInfoFragment {
     public UsersListFragment() {
-        setUserInfoMode(UserInfoMode.PREVIEW);
+        setUserInfoMode(UserInfoMode.ICEBREAKER);
+    }
+
+    @Override
+    public View setViewLayout(View view, LayoutInflater inflater) {
+        view =  inflater.inflate(R.layout.fragment_icebreaker_list, null);
+        return view;
     }
 
     @Override
@@ -14,18 +24,6 @@ public class UsersListFragment extends UserInfoFragment {
         this.usersList.add(user);
 
         user = "Users";
-        usersList.add(user);
-
-        user = "Up";
-        usersList.add(user);
-
-        user = "In";
-        usersList.add(user);
-
-        user = "This";
-        usersList.add(user);
-
-        user = "Place";
         usersList.add(user);
 
         user = "Up";
