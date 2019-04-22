399134833c0dfb3d877d0ad2145feb141bf2d5f9
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-04-22 22:49:20 +0530

    Added View User Activity

---
+++ app/src/main/java/com/frisky/icebreaker/ui/profile/ViewUserActivity.java
@@ -0,0 +1,15 @@
+package com.frisky.icebreaker.ui.profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.frisky.icebreaker.R;
+
+public class ViewUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_user);
+    }
+}
