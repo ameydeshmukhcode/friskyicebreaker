21f373f41fe0240108fbcaabbc76b417892dd573
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-04-25 14:11:20 +0530

    Modified Social Fragment tabs and tab appearance

--- app/src/main/java/com/frisky/icebreaker/ui/social/SocialFragment.java
+++ app/src/main/java/com/frisky/icebreaker/ui/social/SocialFragment.java
@@ -3,6 +3,7 @@ package com.frisky.icebreaker.ui.social;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
@@ -14,9 +15,6 @@ import com.frisky.icebreaker.R;
 
 public class SocialFragment extends Fragment {
 
-    Button mChatsButton;
-    Button mPendingButton;
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -27,22 +25,8 @@ public class SocialFragment extends Fragment {
         SocialPagerAdapter myPagerAdapter = new SocialPagerAdapter(getFragmentManager());
         viewPager.setAdapter(myPagerAdapter);
 
-        mChatsButton = view.findViewById(R.id.button_chats);
-        mPendingButton = view.findViewById(R.id.button_pending);
-
-        mChatsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                viewPager.setCurrentItem(0);
-            }
-        });
-
-        mPendingButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                viewPager.setCurrentItem(1);
-            }
-        });
+        TabLayout tabLayout = view.findViewById(R.id.tab_social);
+        tabLayout.setupWithViewPager(viewPager, true);
 
         return view;
     }
