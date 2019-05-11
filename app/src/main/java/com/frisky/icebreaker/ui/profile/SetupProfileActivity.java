16ea866e00a55e2c3b939df5053fceb5b51c6bb2
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-05-11 19:52:13 +0530

    Added setup profile activity

---
+++ app/src/main/java/com/frisky/icebreaker/ui/profile/SetupProfileActivity.java
@@ -0,0 +1,15 @@
+package com.frisky.icebreaker.ui.profile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.frisky.icebreaker.R;
+
+public class SetupProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup_profile);
+    }
+}
