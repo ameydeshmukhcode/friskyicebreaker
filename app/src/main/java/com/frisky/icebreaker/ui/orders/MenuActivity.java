d098e24d330a4857920f156412592614264db9c4
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-05-14 00:11:05 +0530

    Added Menu and Order activities

---
+++ app/src/main/java/com/frisky/icebreaker/ui/orders/MenuActivity.java
@@ -0,0 +1,39 @@
+package com.frisky.icebreaker.ui.orders;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.frisky.icebreaker.R;
+
+public class MenuActivity extends AppCompatActivity {
+
+    ImageButton mBackButton;
+    Button mViewOrderButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_menu);
+
+        mBackButton = findViewById(R.id.button_back);
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MenuActivity.super.onBackPressed();
+            }
+        });
+
+        mViewOrderButton = findViewById(R.id.button_view_order);
+        mViewOrderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), OrderActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/frisky/icebreaker/ui/orders/OrderActivity.java
@@ -0,0 +1,28 @@
+package com.frisky.icebreaker.ui.orders;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.frisky.icebreaker.R;
+
+public class OrderActivity extends AppCompatActivity {
+
+    ImageButton mBackButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        mBackButton = findViewById(R.id.button_back);
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OrderActivity.super.onBackPressed();
+            }
+        });
+
+    }
+}
