9155a97d625527713223292079fdbeebc8c95059
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-05-18 17:55:26 +0530

    Added logout button

--- app/src/main/java/com/frisky/icebreaker/ui/profile/ProfileActivity.java
+++ app/src/main/java/com/frisky/icebreaker/ui/profile/ProfileActivity.java
@@ -6,10 +6,13 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import com.frisky.icebreaker.R;
+import com.frisky.icebreaker.ui.LoginActivity;
 import com.frisky.icebreaker.ui.SettingsActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -18,6 +21,7 @@ public class ProfileActivity extends AppCompatActivity {
     ProfileImageAdapter mProfileImageAdapter;
     ImageButton mBackButton;
     ImageButton mSettingsButton;
+    Button mLogoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,14 +51,23 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent editProfile = new Intent(getApplicationContext(), SettingsActivity.class);
-                startActivity(editProfile);            }
+                startActivity(editProfile);
+            }
         });
-
 
         mProfileImageAdapter = new ProfileImageAdapter(getApplicationContext());
 
         mProfileImagePager = findViewById(R.id.pager_profile_images);
         mProfileImagePager.setAdapter(mProfileImageAdapter);
+
+        mLogoutButton = findViewById(R.id.button_logout);
+        mLogoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
 
         TabLayout tabLayout = findViewById(R.id.tab_image);
         tabLayout.setupWithViewPager(mProfileImagePager, true);
