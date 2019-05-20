9f126245804503fb1a6ca7242ef61a6707ae779a
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-05-20 21:11:15 +0530

    Added fragment for supporting menu

---
+++ app/src/main/java/com/frisky/icebreaker/orders/MenuFragment.java
@@ -0,0 +1,29 @@
+package com.frisky.icebreaker.orders;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.frisky.icebreaker.R;
+import com.frisky.icebreaker.pubs.PubListFragment;
+
+public class MenuFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view;
+        view = inflater.inflate(R.layout.fragment_menu, null);
+
+        Fragment pubListFragment = new PubListFragment();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_submenu, pubListFragment).commit();
+
+        return view;
+    }
+}
