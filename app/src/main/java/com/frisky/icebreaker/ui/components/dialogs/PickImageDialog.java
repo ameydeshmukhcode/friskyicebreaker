d3c7ddbae4db0cdd4805ee4c9e34e21d595b0be1
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-05-22 02:09:35 +0530

    Added support to select image from gallery and added callback interface

--- app/src/main/java/com/frisky/icebreaker/ui/components/dialogs/PickImageDialog.java
+++ app/src/main/java/com/frisky/icebreaker/ui/components/dialogs/PickImageDialog.java
@@ -1,19 +1,62 @@
 package com.frisky.icebreaker.ui.components.dialogs;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.frisky.icebreaker.R;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import static android.app.Activity.RESULT_OK;
 
 public class PickImageDialog extends DialogFragment {
 
     Button galleryButton;
     Button cameraButton;
+
+    private OnImageUpdatedListener onImageUpdatedListener;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        galleryButton = getDialog().findViewById(R.id.button_gallery);
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent getImageFromDevice = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(getImageFromDevice, 0);
+            }
+        });
+
+//        cameraButton = getDialog().findViewById(R.id.button_camera);
+//        cameraButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent takePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                startActivityForResult(takePicture, 1);
+//            }
+//        });
+    }
 
     @NonNull
     @Override
@@ -23,5 +66,37 @@ public class PickImageDialog extends DialogFragment {
         pickImageDialog.setContentView(R.layout.dialog_pick_image);
 
         return pickImageDialog;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), selectedImage);
+                this.onImageUpdatedListener.imageUpdated(bitmap);
+                dismiss();
+            }
+            catch (IOException exp){
+                Log.e("IOException", "Image not found", exp);
+            }
+        }
+    }
+
+    public static interface OnImageUpdatedListener {
+        public abstract void imageUpdated(Bitmap bitmap);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            this.onImageUpdatedListener = (OnImageUpdatedListener)activity;
+        }
+        catch (final ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement OnImageUpdatedListener");
+        }
     }
 }
