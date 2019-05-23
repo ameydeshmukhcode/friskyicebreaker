6365675a590df4ceaee5f549bc0cb1a9606dd1f4
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-05-23 21:46:23 +0530

    Added error information for failed sign up

--- app/src/main/java/com/frisky/icebreaker/SignUpActivity.java
+++ app/src/main/java/com/frisky/icebreaker/SignUpActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.frisky.icebreaker.ui.base.FormActivity;
@@ -16,6 +18,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 
 public class SignUpActivity extends AppCompatActivity implements FormActivity, UIActivity {
 
@@ -24,6 +29,8 @@ public class SignUpActivity extends AppCompatActivity implements FormActivity, U
     EditText mPasswordInput;
     EditText mConfirmPasswordInput;
     CheckBox mAgeCheck;
+    TextView mErrorText;
+
     FirebaseAuth mAuth;
 
     @Override
@@ -42,6 +49,7 @@ public class SignUpActivity extends AppCompatActivity implements FormActivity, U
         mPasswordInput = findViewById(R.id.input_password);
         mConfirmPasswordInput = findViewById(R.id.input_confirm_password);
         mAgeCheck = findViewById(R.id.checkbox_age);
+        mErrorText = findViewById(R.id.text_error);
 
         mSignUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +60,7 @@ public class SignUpActivity extends AppCompatActivity implements FormActivity, U
     }
 
     private void handleSignUp(String email, String password) {
+        mErrorText.setText("");
         if (validateForm()) {
             mAuth.createUserWithEmailAndPassword(email, password)
                     .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -64,9 +73,21 @@ public class SignUpActivity extends AppCompatActivity implements FormActivity, U
                                 //mAuth.signOut();
                                 Intent startLoginActivity = new Intent(getApplicationContext(), LoginActivity.class);
                                 startActivity(startLoginActivity);
-                            } else {
-                                Toast.makeText(getApplicationContext(), "Failed to create account.",
-                                        Toast.LENGTH_SHORT).show();
+                            } else if (!task.isSuccessful()) {
+                                try {
+                                    throw task.getException();
+                                } catch(FirebaseAuthWeakPasswordException e) {
+                                    Log.e("Create User Error", e.getReason());
+                                    mErrorText.setText(getString(R.string.error_weak_password));
+                                } catch(FirebaseAuthInvalidCredentialsException e) {
+                                    Log.e("Create User Error", e.getErrorCode());
+                                    mErrorText.setText(getString(R.string.error_invalid_email));
+                                } catch(FirebaseAuthUserCollisionException e) {
+                                    Log.e("Create User Error", e.getErrorCode());
+                                    mErrorText.setText(getString(R.string.error_duplicate_email));
+                                } catch(Exception e) {
+                                    Log.e("Create User Error", e.getMessage());
+                                }
                             }
                         }
                     });
