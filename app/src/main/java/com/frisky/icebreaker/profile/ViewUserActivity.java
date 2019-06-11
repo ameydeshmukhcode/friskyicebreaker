5470bad252f3de65c0e9d40a9a44c50d872f2cf6
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-06-12 00:00:36 +0530

    Update to display user bio and profile image in ViewUserActivity

--- app/src/main/java/com/frisky/icebreaker/profile/ViewUserActivity.java
+++ app/src/main/java/com/frisky/icebreaker/profile/ViewUserActivity.java
@@ -1,5 +1,7 @@
 package com.frisky.icebreaker.profile;
 
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
@@ -13,6 +15,15 @@ import android.widget.TextView;
 
 import com.frisky.icebreaker.R;
 import com.frisky.icebreaker.ui.base.UIActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class ViewUserActivity extends AppCompatActivity implements UIActivity {
 
@@ -20,13 +31,23 @@ public class ViewUserActivity extends AppCompatActivity implements UIActivity {
     ProfileImageAdapter mProfileImageAdapter;
     TextView mUserNameText;
     ImageButton mBackButton;
+    TextView mBioText;
 
-    String id;
+    String UID;
+
+    FirebaseStorage mStorage;
+    StorageReference storageReference;
+    FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_user);
+
+        mStorage = FirebaseStorage.getInstance();
+        storageReference = mStorage.getReference();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
         initUI();
     }
 
@@ -43,7 +64,7 @@ public class ViewUserActivity extends AppCompatActivity implements UIActivity {
 
         if (getIntent().hasExtra("id")) {
             String id = getIntent().getStringExtra("id");
-            this.id = id;
+            this.UID = id;
             Log.i("ID", id);
         }
 
@@ -68,6 +89,51 @@ public class ViewUserActivity extends AppCompatActivity implements UIActivity {
                 //TODO ICEBREAKER
                 Snackbar.make(view, "Break Ice through here", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        mBioText = findViewById(R.id.text_bio);
+        DocumentReference docRef = firebaseFirestore.collection("users").document(UID);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document == null)
+                        return;
+                    if (document.exists()) {
+                        mBioText.setText(document.get("bio").toString());
+                        Log.d("Exists", "DocumentSnapshot data: " + document.getData());
+                    }
+                    else {
+                        Log.d("Doesn't exist", "No such document");
+                    }
+                }
+                else {
+                    Log.d("Task", "failed with ", task.getException());
+                }
+            }
+        });
+
+        getProfileImage();
+    }
+
+    private void getProfileImage() {
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+
+        StorageReference profileImageRef = storageReference.child("profile_images").child(UID);
+
+        profileImageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                mProfileImageAdapter.addToImageList(uri);
+                mProfileImageAdapter.notifyDataSetChanged();
+                Log.d("Image Uri downloaded", uri.toString());
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("Uri Download Failed", e.getMessage());
             }
         });
     }
