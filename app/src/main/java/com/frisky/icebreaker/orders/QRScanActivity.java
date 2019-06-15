32d3b40d9cf04a4b1de63da6378fa8fb0322be09
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-06-15 13:18:25 +0530

    Added activity for qr scanning

---
+++ app/src/main/java/com/frisky/icebreaker/orders/QRScanActivity.java
@@ -0,0 +1,15 @@
+package com.frisky.icebreaker.orders;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.frisky.icebreaker.R;
+
+public class QRScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrscan);
+    }
+}
