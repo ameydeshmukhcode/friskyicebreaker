fee7898e9fb975ce8196d7a8fcb1e9ce900f2426
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-06-15 14:52:05 +0530

    Added permission check and enabled qr scanner

--- app/src/main/java/com/frisky/icebreaker/orders/QRScanActivity.java
+++ app/src/main/java/com/frisky/icebreaker/orders/QRScanActivity.java
@@ -1,15 +1,71 @@
 package com.frisky.icebreaker.orders;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
 import com.frisky.icebreaker.R;
+import com.google.zxing.Result;
 
 public class QRScanActivity extends AppCompatActivity {
+
+    public static final int MY_PERMISSIONS_REQUEST_CAMERA = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrscan);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                    MY_PERMISSIONS_REQUEST_CAMERA);
+        }
+        else {
+            setupScannerView();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], @NonNull int[] grantResults) {
+        if (requestCode == MY_PERMISSIONS_REQUEST_CAMERA) {
+            Log.i("Camera", "G : " + grantResults[0]);
+            // If request is cancelled, the result arrays are empty.
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setupScannerView();
+            }
+            else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_CAMERA);
+            }
+        }
+    }
+
+    private void setupScannerView() {
+        CodeScanner mCodeScanner;
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull final Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), result.getText(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+        mCodeScanner.startPreview();
     }
 }
