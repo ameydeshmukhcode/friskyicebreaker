38749869c88fd719921dbb542cd272223bff4dc2
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-06-18 23:31:20 +0530

    Addition to retrieve table details

--- app/src/main/java/com/frisky/icebreaker/orders/MenuActivity.java
+++ app/src/main/java/com/frisky/icebreaker/orders/MenuActivity.java
@@ -20,6 +20,7 @@ public class MenuActivity extends AppCompatActivity implements UIActivity {
 
     ImageButton mBackButton;
     TextView mRestName;
+    TextView mTableSerial;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,36 +48,20 @@ public class MenuActivity extends AppCompatActivity implements UIActivity {
 
         if (getIntent().hasExtra("qr_code_scanned")){
             String restID = "";
+            String tableID = "";
 
             if (getIntent().hasExtra("restaurant_id")) {
                 restID = getIntent().getStringExtra("restaurant_id");
             }
 
+            if (getIntent().hasExtra("table_id")) {
+                tableID = getIntent().getStringExtra("table_id");
+            }
+
             mRestName = findViewById(R.id.text_pub_name);
+            mTableSerial = findViewById(R.id.text_table);
 
-            FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
-
-            DocumentReference docRef = firebaseFirestore.collection("restaurants").document(restID);
-            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                    if (task.isSuccessful()) {
-                        DocumentSnapshot document = task.getResult();
-                        if (document == null)
-                            return;
-                        if (document.exists()) {
-                            mRestName.setText(document.get("name").toString());
-                            Log.i("Exists", "DocumentSnapshot data: " + document.getData());
-                        }
-                        else {
-                            Log.e("Doesn't exist", "No such document");
-                        }
-                    }
-                    else {
-                        Log.e("Task", "failed with ", task.getException());
-                    }
-                }
-            });
+            getRestaurantAndTableDetails(restID, tableID);
 
             MenuFragment menuFragment = new MenuFragment();
 
@@ -88,5 +73,59 @@ public class MenuActivity extends AppCompatActivity implements UIActivity {
                     .replace(R.id.frame_menu, menuFragment)
                     .commit();
         }
+    }
+
+    private void getRestaurantAndTableDetails(String restID, String tableID) {
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+
+        DocumentReference resRef = firebaseFirestore.collection("restaurants").document(restID);
+        resRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document == null)
+                        return;
+                    if (document.exists()) {
+                        mRestName.setText(document.get("name").toString());
+                        Log.i("Exists", "DocumentSnapshot data: " + document.getData());
+                    }
+                    else {
+                        Log.e("Doesn't exist", "No such document");
+                    }
+                }
+                else {
+                    Log.e("Task", "failed with ", task.getException());
+                }
+            }
+        });
+
+        DocumentReference tableRef = firebaseFirestore
+                .collection("restaurants")
+                .document(restID)
+                .collection("tables")
+                .document(tableID);
+
+        tableRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document == null)
+                        return;
+                    if (document.exists()) {
+                        String tableSerial = "Table " + document.get("number");
+                        mTableSerial.setText(tableSerial);
+                        Log.i("Exists", "DocumentSnapshot data: " + document.getData());
+                    }
+                    else {
+                        Log.e("Doesn't exist", "No such document");
+                    }
+                }
+                else {
+                    Log.e("Task", "failed with ", task.getException());
+                }
+            }
+        });
     }
 }
