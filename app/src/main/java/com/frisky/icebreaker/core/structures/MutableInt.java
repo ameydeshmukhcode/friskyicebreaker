c817fddb16f36041804ea159cfe20e8b6ceb48f8
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-07-07 00:11:50 +0530

    Added MutableInt class to store item count in Map

---
+++ app/src/main/java/com/frisky/icebreaker/core/structures/MutableInt.java
@@ -0,0 +1,14 @@
+package com.frisky.icebreaker.core.structures;
+
+public class MutableInt {
+    private int value = 0;
+    public void increment() {
+        ++value;
+    }
+    public void decrement() {
+        --value;
+    }
+    public int getValue() {
+        return value;
+    }
+}
