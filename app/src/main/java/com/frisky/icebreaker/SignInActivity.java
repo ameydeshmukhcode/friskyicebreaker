3059261cf7f35e51d74e57692a6408211d4205d1
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-07-20 11:55:39 +0530

    Removed unused imports

--- app/src/main/java/com/frisky/icebreaker/SignInActivity.java
+++ app/src/main/java/com/frisky/icebreaker/SignInActivity.java
@@ -2,11 +2,9 @@ package com.frisky.icebreaker;
 
 import android.content.Intent;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,8 +13,6 @@ import android.widget.Toast;
 import com.frisky.icebreaker.ui.base.FormActivity;
 import com.frisky.icebreaker.profile.SetupProfileActivity;
 import com.frisky.icebreaker.ui.base.UIActivity;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseNetworkException;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
