0ea2f1a0ad3f2388809ecae60a069ca1ce8aaeba
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-07-28 13:51:03 +0530

    Made OrderItem Serializable

--- app/src/main/java/com/frisky/icebreaker/core/structures/OrderItem.java
+++ app/src/main/java/com/frisky/icebreaker/core/structures/OrderItem.java
@@ -1,6 +1,8 @@
 package com.frisky.icebreaker.core.structures;
 
-public class OrderItem {
+import java.io.Serializable;
+
+public class OrderItem implements Serializable {
     private String id;
     private String name;
     private int count;
