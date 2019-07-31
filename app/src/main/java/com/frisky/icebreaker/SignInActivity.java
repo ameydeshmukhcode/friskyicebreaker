5a0b6133b5d957822e58048b6153043c9eaf9f97
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-08-01 02:31:00 +0530

    Re-enabled Google sign in

--- app/src/main/java/com/frisky/icebreaker/SignInActivity.java
+++ app/src/main/java/com/frisky/icebreaker/SignInActivity.java
@@ -1,31 +1,40 @@
 package com.frisky.icebreaker;
 
 import android.content.Intent;
-
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.frisky.icebreaker.ui.base.FormActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.frisky.icebreaker.profile.SetupProfileActivity;
+import com.frisky.icebreaker.ui.base.FormActivity;
 import com.frisky.icebreaker.ui.base.UIActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseNetworkException;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class SignInActivity extends AppCompatActivity implements FormActivity, UIActivity {
 
-    //private static final int RC_SIGN_IN = 1;
+    private static final int RC_SIGN_IN = 1;
     private int PW_ENTRY_FAILED = 0;
 
     Button mLoginButton;
-    //ImageButton mGoogleButton;
+    ImageButton mGoogleButton;
     TextView mSignUpLink;
     TextView mForgotPasswordLink;
     TextView mErrorText;
@@ -35,7 +44,7 @@ public class SignInActivity extends AppCompatActivity implements FormActivity, U
 
     FirebaseAuth mAuth;
     
-    //GoogleSignInClient mGoogleSignInClient;
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,18 +61,17 @@ public class SignInActivity extends AppCompatActivity implements FormActivity, U
             verifyLogin();
         }
 
-//        Configure Google Sign In
-//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//                .requestIdToken(getString(R.string.default_web_client_id))
-//                .requestEmail()
-//                .build();
-//
-//        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
     }
 
     public void initUI() {
         mLoginButton = findViewById(R.id.button_login);
-//        mGoogleButton = findViewById(R.id.button_google);
+        mGoogleButton = findViewById(R.id.button_google);
         mSignUpLink = findViewById(R.id.link_sign_up);
         mErrorText = findViewById(R.id.text_error);
         mForgotPasswordLink = findViewById(R.id.link_forgot_password);
@@ -71,12 +79,7 @@ public class SignInActivity extends AppCompatActivity implements FormActivity, U
         mEmailInput = findViewById(R.id.input_email);
         mPasswordInput = findViewById(R.id.input_password);
 
-//        mGoogleButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                googleSignIn();
-//            }
-//        });
+        mGoogleButton.setOnClickListener(v -> googleSignIn());
 
         enableForm();
     }
@@ -126,51 +129,48 @@ public class SignInActivity extends AppCompatActivity implements FormActivity, U
         }
     }
 
-//    private void googleSignIn() {
-//        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-//        startActivityForResult(signInIntent, RC_SIGN_IN);
-//    }
-//
-//    @Override
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-//        if (requestCode == RC_SIGN_IN) {
-//            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-//            try {
-//                // Google Sign In was successful, authenticate with Firebase
-//                GoogleSignInAccount account = task.getResult(ApiException.class);
-//                if (account != null) {
-//                    firebaseAuthWithGoogle(account);
-//                }
-//            }
-//            catch (ApiException e) {
-//                // Google Sign In failed, update UI appropriately
-//                Log.w("Warning", "Google sign in failed", e);
-//            }
-//        }
-//    }
-//
-//    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
-//        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-//        mAuth.signInWithCredential(credential)
-//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<AuthResult> task) {
-//                        if (task.isSuccessful()) {
-//                            // Sign in success, update UI with the signed-in user's information
-//                            Intent launchHome = new Intent(getApplicationContext(), HomeActivity.class);
-//                            startActivity(launchHome);
-//                            finish();
-//                        }
-//                        else {
-//                            // If sign in fails, display a message to the user.
-//                            Log.w("Warning", "signInWithCredential:failure", task.getException());
-//                        }
-//                    }
-//                });
-//    }
+    private void googleSignIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                if (account != null) {
+                    firebaseAuthWithGoogle(account);
+                }
+            }
+            catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("Warning", "Google sign in failed", e);
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Intent launchHome = new Intent(getApplicationContext(), HomeActivity.class);
+                        startActivity(launchHome);
+                        finish();
+                    }
+                    else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("Warning", "signInWithCredential:failure", task.getException());
+                    }
+                });
+    }
 
     @Override
     public boolean validateForm() {
