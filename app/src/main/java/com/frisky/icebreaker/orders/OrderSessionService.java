065f4a3285187fa53fc8844cb43cfddd8069aa23
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-08-03 19:41:35 +0530

    Added persistent notification for order updates

--- app/src/main/java/com/frisky/icebreaker/orders/OrderSessionService.java
+++ app/src/main/java/com/frisky/icebreaker/orders/OrderSessionService.java
@@ -1,10 +1,17 @@
 package com.frisky.icebreaker.orders;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
 
 import com.frisky.icebreaker.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -35,6 +42,34 @@ public class OrderSessionService extends Service {
         sharedPreferences = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE);
         addListenerForSessionEnd();
 
+        NotificationManager notificationManager = getSystemService(NotificationManager.class);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (notificationManager.getNotificationChannel(getString(R.string.n_channel_orders)) == null) {
+                int importance = NotificationManager.IMPORTANCE_DEFAULT;
+                NotificationChannel channel = new NotificationChannel(getString(R.string.n_channel_orders),
+                        getString(R.string.n_channel_name_orders), importance);
+                channel.setDescription(getString(R.string.n_channel_desc_orders));
+                // Register the channel with the system; you can't change the importance
+                // or other notification behaviors after this
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+
+        Intent notificationIntent = new Intent(this, MenuActivity.class);
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        NotificationCompat.Builder builder = new
+                NotificationCompat.Builder(this, getString(R.string.n_channel_orders))
+                .setSmallIcon(R.drawable.logo)
+                .setContentTitle("Welcome to " + sharedPreferences.getString("restaurant_name", ""))
+                .setContentText("You're on " + sharedPreferences.getString("table_serial", ""))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent);
+
+        startForeground(1402, builder.build());
     }
 
     @Override
