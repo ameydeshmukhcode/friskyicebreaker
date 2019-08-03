cb43dedc441baac83bd3f70fc2d3f1e1beefc60c
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-08-03 17:22:00 +0530

    Added service for order updates

---
+++ app/src/main/java/com/frisky/icebreaker/orders/OrderService.java
@@ -0,0 +1,16 @@
+package com.frisky.icebreaker.orders;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class OrderService extends Service {
+    public OrderService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
