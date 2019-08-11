02d21df48cc81ee19fb826017b1e731ed0e2d86b
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-08-11 17:08:03 +0530

    Updates to show order list and summary

--- app/src/main/java/com/frisky/icebreaker/orders/OrderSummaryActivity.java
+++ app/src/main/java/com/frisky/icebreaker/orders/OrderSummaryActivity.java
@@ -1,18 +1,27 @@
 package com.frisky.icebreaker.orders;
 
 import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.frisky.icebreaker.R;
+import com.frisky.icebreaker.core.structures.OrderItem;
+import com.frisky.icebreaker.core.structures.OrderStatus;
 import com.frisky.icebreaker.ui.base.UIActivity;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class OrderSummaryActivity extends AppCompatActivity implements UIActivity {
@@ -20,11 +29,21 @@ public class OrderSummaryActivity extends AppCompatActivity implements UIActivit
     TextView mRestaurantName;
     TextView mTableName;
     TextView mOrderDateTime;
+    TextView mFinalTotal;
+
+    RecyclerView mOrderListRecyclerView;
+
+    ArrayList<Object> mOrderList = new ArrayList<>();
+
+    OrderListAdapter orderListAdapter;
+
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_summary);
+        sharedPreferences = getSharedPreferences(getString(R.string.app_name), MODE_PRIVATE);
 
         initUI();
     }
@@ -34,25 +53,39 @@ public class OrderSummaryActivity extends AppCompatActivity implements UIActivit
         mRestaurantName = findViewById(R.id.text_restaurant);
         mTableName = findViewById(R.id.text_table);
         mOrderDateTime = findViewById(R.id.text_date_time);
+        mFinalTotal = findViewById(R.id.text_final_total);
+        mOrderListRecyclerView = findViewById(R.id.recycler_view_final_order);
 
-        if (getIntent().hasExtra("restaurant_name")) {
-            mRestaurantName.setText(getIntent().getStringExtra("restaurant_name"));
+        RecyclerView.LayoutManager mOrderListViewLayoutManager;
+        mOrderListViewLayoutManager = new LinearLayoutManager(getApplicationContext());
+
+        mOrderListRecyclerView.setLayoutManager(mOrderListViewLayoutManager);
+
+        orderListAdapter = new OrderListAdapter(getApplicationContext(), mOrderList);
+        mOrderListRecyclerView.setAdapter(orderListAdapter);
+
+        if (sharedPreferences.contains("R")) {
+            mRestaurantName.setText(sharedPreferences.getString("R", ""));
         }
 
-        if (getIntent().hasExtra("table_name")) {
-            mTableName.setText(getIntent().getStringExtra("table_name"));
+        if (sharedPreferences.contains("T")) {
+            mTableName.setText(sharedPreferences.getString("T", ""));
         }
 
-        if (getIntent().hasExtra("session_id") && getIntent().hasExtra("restaurant_id")) {
-            getOrderSummary(getIntent().getStringExtra("restaurant_id"),
-                    getIntent().getStringExtra("session_id"));
-            Log.d(getString(R.string.tag_debug), getIntent().getStringExtra("restaurant_id") +
-                    getIntent().getStringExtra("session_id"));
+        if (sharedPreferences.contains("S") && sharedPreferences.contains("RID")) {
+            getOrderDetails(sharedPreferences.getString("RID", ""),
+                    sharedPreferences.getString("S", ""));
         }
+
+        sharedPreferences.edit()
+                .remove("R")
+                .remove("T")
+                .remove("RID")
+                .remove("S")
+                .apply();
     }
 
     private void getOrderSummary(String restaurant, String session) {
-
         FirebaseFirestore.getInstance().collection("restaurants").document(restaurant)
                 .collection("sessions").document(session)
                 .get().addOnCompleteListener(task -> {
@@ -67,7 +100,61 @@ public class OrderSummaryActivity extends AppCompatActivity implements UIActivit
                                    .requireNonNull(snapshot.getTimestamp("end_time")).toDate()));
                            mOrderDateTime.setText(endTime);
                        }
+                       if (snapshot.contains("bill_amount")) {
+                           mFinalTotal.setText(String.valueOf(snapshot.get("bill_amount")));
+                       }
                    }
+                });
+    }
+
+    @SuppressWarnings("unchecked")
+    private void getOrderDetails(String restaurantID, String sessionID) {
+        final DocumentReference docRef = FirebaseFirestore.getInstance().collection("restaurants")
+                .document(restaurantID);
+
+        docRef.collection("orders")
+                .whereEqualTo("session_id", sessionID)
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        QuerySnapshot document = task.getResult();
+                        assert document != null;
+
+                        for (DocumentSnapshot snapshot : document.getDocuments()) {
+
+                            Map<String, Object> orderData = (Map<String, Object>) snapshot.get("items");
+                            assert orderData != null;
+
+                            for (Map.Entry<String, Object> entry : orderData.entrySet()) {
+                                String itemID = entry.getKey();
+                                HashMap<String, Object> item = (HashMap<String, Object>) entry.getValue();
+
+                                String name = String.valueOf(item.get("name"));
+                                int count = Integer.parseInt(String.valueOf(item.get("quantity")));
+                                int price = Integer.parseInt(String.valueOf(item.get("cost")));
+                                OrderItem orderItem = new OrderItem(itemID, name, count, (count * price));
+
+                                if (String.valueOf(item.get("status")).equals("pending")) {
+                                    orderItem.setStatus(OrderStatus.PENDING);
+                                }
+                                else if (String.valueOf(item.get("status")).equals("accepted")) {
+                                    orderItem.setStatus(OrderStatus.ACCEPTED);
+                                }
+                                else if (String.valueOf(item.get("status")).equals("rejected")) {
+                                    orderItem.setStatus(OrderStatus.REJECTED);
+                                }
+                                else if (String.valueOf(item.get("status")).equals("cancelled")) {
+                                    orderItem.setStatus(OrderStatus.CANCELLED);
+                                }
+                                mOrderList.add(orderItem);
+                            }
+                        }
+
+                        orderListAdapter = new OrderListAdapter(getApplicationContext(), mOrderList);
+                        mOrderListRecyclerView.setAdapter(orderListAdapter);
+
+                        getOrderSummary(restaurantID, sessionID);
+                    }
                 });
     }
 }
