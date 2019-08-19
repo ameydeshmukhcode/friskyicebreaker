ef5b1db9d84c0e7fc6c5a7c41081b48cc70628dd
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-08-19 20:52:06 +0530

    Renamed to order history

--- app/src/main/java/com/frisky/icebreaker/fragments/OrderHistoryFragment.java
+++ app/src/main/java/com/frisky/icebreaker/fragments/OrderHistoryFragment.java
@@ -17,7 +17,7 @@ public class OrderHistoryFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view;
-        view = inflater.inflate(R.layout.fragment_notifications, container, false);
+        view = inflater.inflate(R.layout.fragment_order_history, container, false);
 
         return view;
     }
