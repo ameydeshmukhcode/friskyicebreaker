e46d22ea81d71c1aa59e1362d6f7a96b2ae0f486
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-08-19 20:50:08 +0530

    Added order history fragment to show list of previous orders

--- app/src/main/java/com/frisky/icebreaker/fragments/OrderHistoryFragment.java
+++ app/src/main/java/com/frisky/icebreaker/fragments/OrderHistoryFragment.java
@@ -1,17 +1,17 @@
 package com.frisky.icebreaker.fragments;
 
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
 import com.frisky.icebreaker.R;
 
-public class NotificationsFragment extends Fragment {
+public class OrderHistoryFragment extends Fragment {
 
     @Nullable
     @Override
