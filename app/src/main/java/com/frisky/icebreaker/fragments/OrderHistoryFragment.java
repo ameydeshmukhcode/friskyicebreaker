64170f0df21fcae61203ba2dbc8818ef97a830c3
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-08-19 22:23:27 +0530

    Update to retrieve order history for current user and add to list

--- app/src/main/java/com/frisky/icebreaker/fragments/OrderHistoryFragment.java
+++ app/src/main/java/com/frisky/icebreaker/fragments/OrderHistoryFragment.java
@@ -1,6 +1,8 @@
 package com.frisky.icebreaker.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,17 +10,74 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.frisky.icebreaker.R;
+import com.frisky.icebreaker.core.structures.OrderSummary;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class OrderHistoryFragment extends Fragment {
+
+    private List<OrderSummary> mSessionHistoryList = new ArrayList<>();
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view;
-        view = inflater.inflate(R.layout.fragment_order_history, container, false);
+        View view = inflater.inflate(R.layout.fragment_order_history, container, false);
+
+        getOrderHistory();
+
+        RecyclerView mRecyclerPubView;
+        mRecyclerPubView = view.findViewById(R.id.recycler_view);
+
+        RecyclerView.LayoutManager mPubViewLayoutManager;
+        // use a linear layout manager
+        mPubViewLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerPubView.setLayoutManager(mPubViewLayoutManager);
 
         return view;
+    }
+
+    private void getOrderHistory() {
+        FirebaseFirestore mFirestore = FirebaseFirestore.getInstance();
+
+        mFirestore.collectionGroup("sessions")
+                .whereEqualTo("created_by", Objects.requireNonNull(FirebaseAuth.getInstance()
+                        .getCurrentUser()).getUid())
+                .orderBy("end_time", Query.Direction.DESCENDING)
+                .get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                if (task.getResult() != null) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        if (document == null)
+                            return;
+                        @SuppressLint("SimpleDateFormat")
+                        SimpleDateFormat formatter = new SimpleDateFormat("dd MMM YYYY hh:mm a");
+                        String endTime = formatter.format((Objects
+                                .requireNonNull(document.getTimestamp("end_time")).toDate()));
+                        final DocumentReference restaurantReference = document.getReference().getParent().getParent();
+                        restaurantReference.get().addOnCompleteListener(restTask -> {
+                            OrderSummary summary = new OrderSummary(restaurantReference.getId(),
+                                    String.valueOf(restTask.getResult().get("name")),
+                            document.getId(), endTime);
+                            mSessionHistoryList.add(summary);
+                        });
+                    }
+                }
+            }
+            else {
+                Log.e("error", "Error getting documents: ", task.getException());
+            }
+        });
     }
 }
