29b3bb9e9313222256e687506aa20a0a950cf3db
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-08-22 01:44:41 +0530

    Added listener to scan qr button to launch QR Code scanner

--- app/src/main/java/com/frisky/icebreaker/fragments/DiningFragment.java
+++ app/src/main/java/com/frisky/icebreaker/fragments/DiningFragment.java
@@ -1,15 +1,18 @@
 package com.frisky.icebreaker.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.frisky.icebreaker.R;
+import com.frisky.icebreaker.activities.QRScanActivity;
 
 public class DiningFragment extends Fragment {
 
@@ -17,6 +20,12 @@ public class DiningFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_dining_empty_state, container, false);
+
+        Button scanQRCodeButton = view.findViewById(R.id.button_scan_qr);
+        scanQRCodeButton.setOnClickListener(v -> {
+            startActivity(new Intent(getActivity(), QRScanActivity.class));
+        });
+
         return view;
     }
 }
