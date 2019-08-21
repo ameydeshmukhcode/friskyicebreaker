6bc3f6308915a26d11811da8b0860d60e4682ddb
Améy Deshmukh <AmeyD-renewed@users.noreply.github.com>
2019-08-22 01:32:32 +0530

    Added DiningFragment

---
+++ app/src/main/java/com/frisky/icebreaker/fragments/DiningFragment.java
@@ -0,0 +1,22 @@
+package com.frisky.icebreaker.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.frisky.icebreaker.R;
+
+public class DiningFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_dining_empty_state, container, false);
+        return view;
+    }
+}
